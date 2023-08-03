548457b01a46caafac5ad99f648fa0d54f4d41f6
GerinAryoPrasetia <gerin.aryo@efishery.com>
2023-08-03 21:33:31 +0700

    add readme

---
+++ find-pi/Readme.md
@@ -0,0 +1,5 @@
+
+# Find PI to the Nth Digit
+
+the Program that generate PI up to that many decimal places.
+
--- find-pi/main.go
+++ find-pi/main.go
@@ -7,11 +7,14 @@ import (
 
 const pi = math.Pi
 
-func findPi(val int) float64 {
+func findPi(val int) (float64, error) {
 	ratio := math.Pow(10, float64(val))
-	return math.Round(pi*ratio) / ratio
+	if val > 15 {
+		return 0, fmt.Errorf("value must be less than 15")
+	}
+	return math.Round(pi*ratio) / ratio, nil
 }
 
 func main() {
-	fmt.Println(findPi(10))
+	fmt.Println(findPi(20))
 }
